186610c64814a63d70203b4f2924561691dbf1a0
yelo12324 <yeloindia.contact@gmail.com>
2025-08-12 20:31:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -370,7 +370,7 @@ app.post('/memberZone/business', (req, res) => {
 
 
 // ---------- START SERVER ----------
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
 });
